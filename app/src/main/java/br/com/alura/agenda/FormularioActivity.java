4f3ce5e873ecbda14693d4d202c0d841511254a1
Renan Saraiva <renan.saraiva@gmail.com>
2017-06-26 14:07:47 -0300

    Fim da aula 03

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -3,8 +3,12 @@ package br.com.alura.agenda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
@@ -13,14 +17,38 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+    }
 
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
                 Toast.makeText(FormularioActivity.this, "Botão Clicado!", Toast.LENGTH_SHORT).show();
+
+                EditText campoNome = (EditText) findViewById(R.id.formulario_nome);
+                String nome = campoNome.getText().toString();
+
+                EditText campoEndereco = (EditText) findViewById(R.id.formulario_endereco);
+                String endereco = campoEndereco.getText().toString();
+
+                EditText campoTelefone = (EditText) findViewById(R.id.formulario_telefone);
+                String telefone = campoTelefone.getText().toString();
+
+                EditText campoSite = (EditText) findViewById(R.id.formulario_site);
+                String site = campoSite.getText().toString();
+
                 finish();
-            }
-        });
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -15,7 +15,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos = {"Daniel", "Ronaldo", "Filipe", "Renan"};
+        String[] alunos = {"Daniel", "Ronaldo", "Filipe", "Renan", "Daniel", "Ronaldo", "Filipe", "Renan", "Daniel", "Ronaldo", "Filipe", "Renan"};
         ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
